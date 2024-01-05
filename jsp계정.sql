b192897892596df176cbad12df5c9570e7554cd9
YS216 <kwon0seo@gmail.com>
2024-01-05 11:33:53 +0900

    24.01.05

--- jsp계정.sql
+++ jsp계정.sql
@@ -16,3 +16,23 @@ INSERT INTO MEMBER VALUES('lee', '1234', '이길동', '2', '231115', 'lee@naver.
 INSERT INTO MEMBER VALUES('park', '1234', '박길동', '1', '231021', 'park@naver.com', '34567', '경기도 성남시 수정구', '수정아파트','01010','공무원');
 
 commit;
+
+CREATE TABLE votelist (
+  num number PRIMARY KEY,
+  question varchar2(200) NOT NULL,
+  sdate date,
+  edate date,
+  wdate date,
+  type number DEFAULT 1 NOT NULL,
+  active number DEFAULT 1
+);
+
+CREATE TABLE voteitem (
+  listnum NUMBER,
+  itemnum NUMBER DEFAULT 0,
+  item varchar2(50),
+  count NUMBER DEFAULT 0,
+  PRIMARY KEY (listnum, itemnum)
+); 
+
+CREATE SEQUENCE SEQ_VOTE;
